aafc92ca8cdaf13eb5d03385f9946e4b49907550
Fatir Ahmad <djfdat@gmail.com>
2016-05-10 23:44:19 -0700

    Created update phaser batch file

---
+++ update_phaser.bat
@@ -0,0 +1,9 @@
+curl ^
+-o bin/js/phaser.js https://raw.githubusercontent.com/photonstorm/phaser/master/build/phaser.js ^
+-o bin/js/phaser.map https://raw.githubusercontent.com/photonstorm/phaser/master/build/phaser.map ^
+-o bin/js/phaser.min.js https://raw.githubusercontent.com/photonstorm/phaser/master/build/phaser.min.js ^
+-o tsd/p2.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/typescript/p2.d.ts ^
+-o tsd/phaser.comments.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/typescript/phaser.comments.d.ts ^
+-o tsd/phaser.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/typescript/phaser.d.ts ^
+-o tsd/pixi.comments.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/typescript/pixi.comments.d.ts ^
+-o tsd/pixi.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/typescript/pixi.d.ts
