0587219c189001d3f3a0d881aff611dda6a99bab
Lauri Härsilä <murgo@iki.fi>
2016-12-14 00:32:32 +0200

    Added new .d.ts files to update scripts, closes #7

--- update_phaser.bat
+++ update_phaser.bat
@@ -2,8 +2,10 @@ curl ^
 -o bin/js/phaser.js https://raw.githubusercontent.com/photonstorm/phaser/master/v2-community/build/phaser.js ^
 -o bin/js/phaser.map https://raw.githubusercontent.com/photonstorm/phaser/master/v2-community/build/phaser.map ^
 -o bin/js/phaser.min.js https://raw.githubusercontent.com/photonstorm/phaser/master/v2-community/build/phaser.min.js ^
+-o tsd/box2d.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/v2-community/typescript/box2d.d.ts ^
 -o tsd/p2.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/v2-community/typescript/p2.d.ts ^
 -o tsd/phaser.comments.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/v2-community/typescript/phaser.comments.d.ts ^
 -o tsd/phaser.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/v2-community/typescript/phaser.d.ts ^
+-o tsd/phaser_box2d.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/v2-community/typescript/phaser_box2d.d.ts ^
 -o tsd/pixi.comments.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/v2-community/typescript/pixi.comments.d.ts ^
 -o tsd/pixi.d.ts https://raw.githubusercontent.com/photonstorm/phaser/master/v2-community/typescript/pixi.d.ts
